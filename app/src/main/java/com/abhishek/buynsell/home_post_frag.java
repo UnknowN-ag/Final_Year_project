53958896409dd346db8206085d444224c59563f9
UnknowN-ag <ag528927@gmail.com>
2019-08-05 21:59:51 +0530

    splash screen

--- app/src/main/java/com/abhishek/buynsell/home_post_frag.java
+++ app/src/main/java/com/abhishek/buynsell/home_post_frag.java
@@ -1,18 +1,32 @@
 package com.abhishek.buynsell;
 
+import android.Manifest;
 import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.core.content.ContextCompat;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.Spinner;
+import android.widget.Toast;
 
 
 public class home_post_frag extends Fragment {
+
+    ImageView imageView;
+
+    private static final int IMAGE_PICK_CODE = 1000;
+    private static final int PERMISSION_CODE = 1001;
 
 
     public home_post_frag() {
@@ -31,8 +45,67 @@ public class home_post_frag extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home_post_frag, container, false);
+        View view = inflater.inflate(R.layout.fragment_home_post_frag, container, false);
+
+        imageView = view.findViewById(R.id.post_img);
+
+        imageView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //  check runtime permission
+                if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.M){
+                    if(ContextCompat.checkSelfPermission(getActivity(),Manifest.permission.READ_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED){
+                        //permission not granted, request for permission
+                        String[] permission = {Manifest.permission.READ_EXTERNAL_STORAGE};
+                        //show popup
+                        requestPermissions(permission, PERMISSION_CODE);
+                    }else{
+                        //permission already granted
+                        pickImgFromGallery();
+                    }
+
+                }else{
+                    // verion less then marshmallow
+                    pickImgFromGallery();
+                }
+
+            }
+        });
+        return  view;
     }
 
+    private void pickImgFromGallery(){
+        //intent to pic img
+        Intent intent = new Intent(Intent.ACTION_PICK);
+        intent.setType("image/*");
+        startActivityForResult(Intent.createChooser(intent, "Select Picture"), IMAGE_PICK_CODE);
+    }
 
+    // handle result of runtime permisiion
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        switch (requestCode){
+            case PERMISSION_CODE:{
+                if(grantResults.length>0 && grantResults[0] == PackageManager.PERMISSION_GRANTED){
+                    pickImgFromGallery();
+                }
+                else{
+                    Toast.makeText(getActivity(),"Permission Denied", Toast.LENGTH_SHORT).show();
+                }
+            }
+        }
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if(resultCode == getActivity().RESULT_OK ){
+             switch (requestCode){
+               case IMAGE_PICK_CODE:
+        //data.getData returns the content URI for the selected Image
+               Uri selectedImage = data.getData();
+               imageView.setImageURI(selectedImage);
+               break;
+         }
+        }
+    }
 }
